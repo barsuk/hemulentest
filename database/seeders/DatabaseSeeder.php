eeb1df11896e4fc620fa46cf1d02c3c6c3c3ec05
Alina <alina.seryodkina@gmail.com>
2022-04-03 20:46:05 +0300

    add: seed users
    https://github.com/linacoder/hemulentest/issues/1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,9 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use Illuminate\Support\Facades\DB;
+use Illuminate\Support\Facades\Hash;
+use Illuminate\Support\Str;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +17,21 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+        $seeded = '';
+        foreach (range(0,1) as $n) {
+            $name = Str::random(10);
+            $email = Str::random(10).'@hemulen.it';
+            $password = Str::random(8);
+
+            DB::table('users')->insert([
+                'name' => $name,
+                'email' => $email,
+                'password' => Hash::make($password),
+            ]);
+            $seeded .= sprintf("name: %s\nemail:%s\npassword: %s\n", $name, $email, $password);
+        }
+        file_put_contents("seed_users.txt",
+            $seeded, FILE_APPEND);
+        echo PHP_EOL . "PREDEFINED USERS:" . PHP_EOL . $seeded . PHP_EOL;
     }
 }
